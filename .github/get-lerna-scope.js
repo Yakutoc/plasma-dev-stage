86998b651a74409a07c63082388db3c1a9d9a9f2
Alexander Lobyntsev <yakutoc@gmail.com>
2024-03-25 13:36:10 +0700

    fix: test pr

--- .github/get-lerna-scope.js
+++ .github/get-lerna-scope.js
@@ -1,9 +1,39 @@
 const META = require('./meta');
 
 module.exports = () => {
-    const { PROCESSED_DATA, PACKAGE } = process.env;
+    const { PACKAGE } = process.env;
 
-    const processedData = JSON.parse(PROCESSED_DATA);
+    const processedData = [
+        'caldera-online',
+        'caldera',
+        'plasma-asdk',
+        'plasma-b2c',
+        'plasma-core',
+        'plasma-hope',
+        'plasma-new-hope',
+        'plasma-tokens-b2b',
+        'plasma-tokens-b2c',
+        'plasma-tokens-web',
+        'plasma-tokens',
+        'plasma-ui',
+        'plasma-web',
+        'sdds-serv',
+        'caldera-online-themes',
+        'data-themes',
+        'flamingo-themes',
+        'plasma-themes',
+        'sdds-themes',
+        'plasma-cy-utils',
+        'plasma-docs-ui',
+        'plasma-sb-utils',
+        'plasma-tokens-native',
+        'plasma-tokens-utils',
+        'plasma-theme-builder',
+        'plasma-ui-docs',
+        'plasma-web-docs',
+        'plasma-website',
+        'sdds-serv-docs',
+    ];
 
     const packageScope = META[PACKAGE].scope || [];
     const requiredDeps = META[PACKAGE]['required-deps'] || [];
