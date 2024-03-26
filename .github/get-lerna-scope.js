2a5f5a217e973c40c215efb2593e7df841edf3a0
Alexander Lobyntsev <yakutoc@gmail.com>
2024-03-26 12:17:53 +0700

    fix: test pr

--- .github/get-lerna-scope.js
+++ .github/get-lerna-scope.js
@@ -3,6 +3,7 @@ const META = require('./meta');
 module.exports = () => {
     const { PACKAGE } = process.env;
 
+    // INFO: результат команды `lerna la --since` - список пакетов в которых были изменения
     const processedData = [
         'caldera-online',
         'caldera',
@@ -35,23 +36,35 @@ module.exports = () => {
         'sdds-serv-docs',
     ];
 
+    // INFO: Список пакетов которые нужно поставить если они есть в списке processedData
     const packageScope = META[PACKAGE].scope || [];
+    const packageTheme = META[PACKAGE].themes;
+
+    // INFO: Список обязательных пакетов без которых не будет работать сборка
+    // INFO: Например для корректной сборки storybook в `plasma-asdk` нужно ставить plasma-core
+    // INFO: Это workaround - временно позволяет обойти ошибки в инфре
     const requiredDeps = META[PACKAGE]['required-deps'] || [];
 
-    const deps = [...packageScope, `${PACKAGE}-docs`, 'plasma-icons', 'plasma-sb-utils', 'data-themes'];
+    // INFO: Пакет с документацией, например `plasma-web-docs` или `sdds-serv-docs`
+    const packageDocs = `${PACKAGE}-docs`;
 
-    const computedScope = processedData.filter((dep) => deps.includes(dep));
+    const computedScope = processedData.filter((dep) => [...packageScope, packageDocs].includes(dep));
 
-    const scope = [...computedScope, ...requiredDeps, PACKAGE, 'plasma-docs-ui', 'plasma-typo'];
+    const scope = new Set([...computedScope, ...requiredDeps, PACKAGE, 'plasma-docs-ui']);
+
+    if (scope.has(packageTheme)) {
+        scope.add('data-themes');
+        scope.add('plasma-typo');
+    }
 
     // TODO: Как избавиться от этой зависимости
     if (processedData.includes('plasma-tokens-native')) {
-        scope.push('plasma-tokens');
-        scope.push('plasma-tokens-utils');
-        scope.push('plasma-tokens-native');
+        scope.add('plasma-tokens');
+        scope.add('plasma-tokens-utils');
+        scope.add('plasma-tokens-native');
     }
 
-    console.log('Result =>', scope);
+    console.log('Result =>', Array.from(scope));
 
-    return `@salutejs/{${scope.join(',')}}`;
+    return `@salutejs/{${Array.from(scope).join(',')}}`;
 };
