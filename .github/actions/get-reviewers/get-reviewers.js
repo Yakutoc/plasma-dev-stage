7b0b81ac7d220ccac2c9d01964e3181742afe976
Alexander Lobyntsev <yakutoc@gmail.com>
2024-07-23 13:29:14 +0700

    ci: PR Ready for Review WIP [no ci]

---
+++ .github/actions/get-reviewers/get-reviewers.js
@@ -0,0 +1,50 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+async function run() {
+    try {
+        const token = core.getInput('token');
+
+        const octokit = new github.getOctokit(token);
+        const context = github.context;
+
+        if (context.payload.pull_request == null) {
+            core.setFailed('No pull request found.');
+
+            return;
+        }
+
+        // Получение последних 15 pull запросов
+        const pulls = await octokit.pulls.list({
+            owner: 'salute-developers',
+            repo: 'plasma',
+            per_page: 15,
+        });
+
+        for (const pull of pulls.data) {
+            const reviewers = pull.requested_reviewers.map((user) => user.login);
+
+            console.log(`Pull Request #${pull.number}: Reviewers: ${reviewers.join(', ')}`);
+        }
+
+        // const pullRequestNumber = context.payload.pull_request.number;
+
+        await octokit.pulls.requestReviewers({
+            ...context.repo,
+            pull_number: context.payload.pull_request.number,
+            reviewers: ['neretin-trike'],
+        });
+
+        // const params = {
+        //     ...context.repo,
+        //     pull_number: pullRequestNumber,
+        //     reviewers: [],
+        // };
+        //
+        // octokit.pulls.requestReviewers(params);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+run();
