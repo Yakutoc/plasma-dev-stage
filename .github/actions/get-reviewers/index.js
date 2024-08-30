354c1016e260fedfbddd7693870a52d44dd0c273
Alexander Lobyntsev <yakutoc@gmail.com>
2024-08-30 12:29:32 +0700

    ci: Set reviewers WIP [no ci]

--- .github/actions/get-reviewers/index.js
+++ .github/actions/get-reviewers/index.js
@@ -1,5 +1,37 @@
+const fs = require('node:fs/promises');
 const core = require('@actions/core');
 const github = require('@actions/github');
+
+const teammates = require('../../teammates.json');
+
+const getRandomIndex = (arr) => Math.floor(Math.random() * arr.length);
+
+function getRandomReviewers(teammates) {
+    if (!teammates || teammates.length === 0 || teammates.length < 2) {
+        return teammates;
+    }
+
+    const selectedKeys = [];
+
+    for (let i = 0; i < 2; ) {
+        const randomIndex = getRandomIndex(teammates);
+        const randomKey = teammates[randomIndex];
+
+        if (!selectedKeys.includes(randomKey)) {
+            selectedKeys.push(randomKey);
+            i++;
+        }
+    }
+
+    return selectedKeys;
+}
+
+const reviewersFormatter = (acc, member) => {
+    acc.mm_list.push(`@${teammates[member]}`);
+    acc.gh_list.push(member);
+
+    return acc;
+};
 
 async function run() {
     try {
@@ -14,46 +46,66 @@ async function run() {
             return;
         }
 
-        // Получение последних 15 pull запросов
-        const pulls = await octokit.rest.pulls.list({
-            owner: 'salute-developers',
-            repo: 'plasma',
-            per_page: 15,
-            state: 'open',
+        const authorPullRequest = context.payload.pull_request.user.login || 'Yakutoc';
+
+        // INFO: Собираем все открытые pull request которые готовы к review и где автор не бот
+        // INFO: Запись формата `-draft:VALUE` значит исключить по условию
+        const {
+            data: { items },
+        } = await octokit.rest.search.issuesAndPullRequests({
+            q: `repo:salute-developers/plasma is:pr is:open review:required -draft:true -author:renovate[bot]`,
+            per_page: 10,
         });
 
-        const data = pulls.data
-            .filter(({ draft }) => !draft)
-            .flatMap(({ requested_reviewers }) => requested_reviewers.map(({ login }) => login));
+        const listPullRequests = items.map(({ number }) => number);
 
-        const data2 = data.reduce((acc, reviewer) => {
-            acc.set(reviewer, acc.has(reviewer) ? acc.get(reviewer) + 1 : 1);
+        // INFO: Получаем список участников для review для каждого pull request
+        // INFO: Once a requested reviewer submits a review, they are no longer considered a requested reviewer.
+        // INFO: Their review will instead be returned by the List reviews for a pull request operation.
+        const listRequestedReviewers = (
+            await Promise.all(
+                listPullRequests.map(async (number) => {
+                    const {
+                        data: { users },
+                    } = await octokit.rest.pulls.listRequestedReviewers({
+                        owner: 'salute-developers',
+                        repo: 'plasma',
+                        pull_number: number,
+                    });
+
+                    return users.map(({ login }) => ({ id: number, reviewer: login }));
+                }),
+            )
+        ).flat();
+
+        // INFO: Считаем кол-во участия для каждого участника review
+        // INFO: Пример вывода, Map(3) { TitanKuzmich => 2, shuga2704 => 1, Yakutoc => 0 }
+        const mapReviewersByCount = listRequestedReviewers.reduce((acc, { reviewer }) => {
+            const count = acc.has(reviewer) ? acc.get(reviewer) + 1 : 1;
+
+            acc.set(reviewer, count);
 
             return acc;
         }, new Map());
 
-        // for (const reviewers of data) {
-        //     console.log(`Reviewers: ${reviewers.join(', ')}`);
-        // }
+        // INFO: Исключаем из списка автора pull request и тех кто уже назначен на review больше 2 раз
+        const reviewers = Object.keys(teammates)
+            .filter((key) => {
+                return mapReviewersByCount.has(key) ? mapReviewersByCount.get(key) < 2 : true;
+            })
+            .filter((key) => key !== authorPullRequest);
 
-        console.log('DATA => ', data);
-        console.log('DATA 2 => ', data2);
+        if (!reviewers.length) {
+            return { mm_list: ['@alex_czech'], gh_list: ['Yakutoc'] };
+        }
 
-        // const pullRequestNumber = context.payload.pull_request.number;
+        if (reviewers.length <= 2) {
+            return reviewers.reduce(reviewersFormatter, { mm_list: [], gh_list: [] });
+        }
 
-        await octokit.rest.pulls.requestReviewers({
-            ...context.repo,
-            pull_number: context.payload.pull_request.number,
-            reviewers: ['neretin-trike'],
-        });
+        const randomReviewers = getRandomReviewers(reviewers);
 
-        // const params = {
-        //     ...context.repo,
-        //     pull_number: pullRequestNumber,
-        //     reviewers: [],
-        // };
-        //
-        // octokit.pulls.requestReviewers(params);
+        return randomReviewers.reduce(reviewersFormatter, { mm_list: [], gh_list: [] });
     } catch (error) {
         core.setFailed(error.message);
     }
